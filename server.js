6f1fe205c80cc330fd3ff7b4cb020b76ded71cac
nilsong87 <nilsonjosesilvagomes1@gmail.com>
2025-05-20 15:35:41 -0300

    back-end8

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Permite qualquer origem (CORS liberado)
+// Força CORS para qualquer origem e métodos
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
