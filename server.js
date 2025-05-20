c77eb22321a62e3ec28d4451501403e64dca15ee
nilsong87 <nilsonjosesilvagomes1@gmail.com>
2025-05-20 15:12:17 -0300

    back-end4

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://nilsong87.github.io', 'https://first-wealthy-rest.glitch.me']
+}));
 app.use(express.json());
 
 // Use a pasta .data para persistência no Glitch
